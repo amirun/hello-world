8a3536088abe91ef26988dd90d5d43d6ec31cd7d
amirun <amirunb007@gmail.com>
2015-12-26 18:08:26 +0000

    First commit

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,11 @@
+class ApplicationController < ActionController::Base
+  # Prevent CSRF attacks by raising an exception.
+  # For APIs, you may want to use :null_session instead.
+  protect_from_forgery with: :exception
+  
+  #added 26DEC,15 8:24PM
+  def hello
+    render text: "hello world!!!"
+  end
+  
+end
